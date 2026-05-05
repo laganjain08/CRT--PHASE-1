6d3dbebc4d3bf269561f66f9536e5c10890a89ff
laganjain08 <abhalagan2005@gmail.com>
2026-05-05 09:42:08 +0530

    type4

---
+++ c++/Day 5/type3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int add(int a,int b){
+    return a+b;
+}
+int main(){
+    int sum=add(5,5);
+    cout<<"Sum is"<<sum<<endl;
+    return 0;
+}
---
+++ c++/Day 5/type4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+string msg(){
+    return "Hello";
+}
+int main(){
+    string ans=msg();
+    cout<<ans<<endl;
+    return 0;
+}
