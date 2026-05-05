68cb2e59822af057b8545daa69441fe067cc375b
laganjain08 <abhalagan2005@gmail.com>
2026-05-05 09:23:38 +0530

    function

---
+++ c++/Day 5/function.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+void printmsg(){//define the function
+    cout<<"Hello World"<<endl;//declare the function
+}
+int main(){
+    printmsg();//call the function
+    return 0;
+}
