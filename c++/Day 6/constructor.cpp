9b4efb8119743c6f6dc3eb5e8622994264df6ac6
laganjain08 <abhalagan2005@gmail.com>
2026-05-06 10:32:44 +0530

    normal

---
+++ c++/Day 6/constructor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ class Student{
+    public:
+    string name;//data member
+    int rollno;
+    
+    //Default Constructor
+    Student(){
+        name="Lagan";
+        rollno=1;
+
+    }
+    void show(){
+        cout<<"Name:"<<name<<endl;
+        cout<<"Roll no:"<<rollno<<endl;
+        cout<<"Default Constructor is called"<<endl;
+    }
+ };
+ int main(){
+    Student s1;
+    s1.show();
+    s1.name;
+    s1.rollno;
+    return 0;
+ }
